efaebe615456d2daad61aa71d199685d10994376
kartik0603 <kartikhirapara800@gmail.com>
2024-06-13 22:25:35 +0530

    Project 1 Intial commit

---
+++ components/tabnavigation.js
@@ -0,0 +1,34 @@
+  
+function openTab(event, tabId) {
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove("active");
+    }
+
+   
+    document.getElementById(tabId).style.display = "block";
+
+    event.currentTarget.classList.add("active");
+
+   
+    localStorage.setItem("selectedTab", tabId);
+}
+
+function loadDefaultTab() {
+    var defaultTabId = localStorage.getItem("selectedTab") || "tab1"; 
+    document.getElementById(defaultTabId).style.display = "block"; 
+    document.querySelector(".tablinks[data-tab-id='" + defaultTabId + "']").classList.add("active"); 
+}
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    loadDefaultTab();
+});
